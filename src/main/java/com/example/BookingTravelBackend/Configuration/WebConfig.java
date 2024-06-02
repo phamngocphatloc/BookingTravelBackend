b80368a2190f6c908fa74cfa85ce0bdfee625be0
phamngocphatloc <phamngocphatloc@gmail.com>
2024-06-02 22:54:36 +0700

    fix api booking

--- src/main/java/com/example/BookingTravelBackend/Configuration/WebConfig.java
+++ src/main/java/com/example/BookingTravelBackend/Configuration/WebConfig.java
@@ -1,6 +1,6 @@
 package com.example.BookingTravelBackend.Configuration;
 
 public class WebConfig {
-    public static String url = "http://127.0.0.1:5050";
+    public static String url = "http://127.0.0.1:5500";
     public static String urlBackend = "http://localhost:8080";
 }
