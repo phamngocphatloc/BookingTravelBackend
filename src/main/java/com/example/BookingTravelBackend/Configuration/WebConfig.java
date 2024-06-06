e9a7fbc02b761abca2bf861013bec1a05c29a298
phamngocphatloc <146187674+phamngocphatloc@users.noreply.github.com>
2024-06-06 21:38:42 +0700

    add api admin

--- src/main/java/com/example/BookingTravelBackend/Configuration/WebConfig.java
+++ src/main/java/com/example/BookingTravelBackend/Configuration/WebConfig.java
@@ -2,5 +2,5 @@ package com.example.BookingTravelBackend.Configuration;
 
 public class WebConfig {
     public static String url = "http://bookingtravel.click";
-    public static String urlBackend = "http://103.69.87.92:8080";
+    public static String urlBackend = "https://bookingapi.click";
 }
