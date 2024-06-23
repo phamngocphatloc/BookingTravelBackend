0050b50c485d5b9a3dbb8f559b51a27ce11e0c93
phamngocphatloc <146187674+phamngocphatloc@users.noreply.github.com>
2024-06-23 15:33:40 +0700

    add search hotel by hotelName

--- src/main/java/com/example/BookingTravelBackend/Repository/HotelPartnersRepository.java
+++ src/main/java/com/example/BookingTravelBackend/Repository/HotelPartnersRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @Repository
 public interface HotelPartnersRepository extends JpaRepository<HotelPartners,Integer> {
-    @Query (value = "select top 5 hotel_name from hotel_partners where partners_id in (select partners_id from hotel join tourist_attraction on hotel.tourist_attaction_id = tourist_attraction.id where tourist_attraction.name like N'Thành Phố Hồ Chí Minh') and hotel_name like ?2)",nativeQuery = true)
+    @Query (value = "select top 5 hotel_name from hotel_partners where partners_id in (select partners_id from hotel join tourist_attraction on hotel.tourist_attaction_id = tourist_attraction.id \n" +
+            "where tourist_attraction.name like ?1) \n" +
+            "and hotel_name like ?2",nativeQuery = true)
     public List<String> findHotelNameByTour (String tour, String hotelNameFind);
 }
