03731195c3123ed311d1da0e779d1581ad728179
PC <phamngocphatloc@gmail.com>
2024-11-05 21:28:30 +0700

    fix cart

--- src/main/java/com/example/BookingTravelBackend/payload/respone/CartDetailsResponse.java
+++ src/main/java/com/example/BookingTravelBackend/payload/respone/CartDetailsResponse.java
@@ -9,10 +9,11 @@ public class CartDetailsResponse {
     private int id;
     private int amount;
     private MenuRestaurantResponse product;
-
+    private String size;
     public CartDetailsResponse (CartDetails cart){
         this.id = cart.getId();
         this.amount = cart.getAmount();
         this.product = new MenuRestaurantResponse(cart.getProduct());
+        this.size = cart.getSize();
     }
 }
