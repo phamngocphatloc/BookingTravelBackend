5977a49e854bed4c055cd0fe3a4697257faf04dc
PC <phamngocphatloc@gmail.com>
2024-11-09 20:30:35 +0700

    fixvnpay

--- src/main/java/com/example/BookingTravelBackend/Configuration/WebConfig.java
+++ src/main/java/com/example/BookingTravelBackend/Configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.BookingTravelBackend.Configuration;
 
 public class WebConfig {
-    public static String url = "http://bookingtravel.click";
+    public static String url = "http://travelbook.com.vn";
     public static String urlBackend = "https://bookingapi.click";
 }
+
