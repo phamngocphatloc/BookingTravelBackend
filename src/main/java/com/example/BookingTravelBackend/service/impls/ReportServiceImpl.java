3e1186709bfa6d488970734a3486e4ef9aba1b63
thaomilo2607 <trongthao0000@gmail.com>
2024-12-13 16:52:13 +0700

    reportprocessing

--- src/main/java/com/example/BookingTravelBackend/service/impls/ReportServiceImpl.java
+++ src/main/java/com/example/BookingTravelBackend/service/impls/ReportServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.BookingTravelBackend.service.impls;
 
+import com.example.BookingTravelBackend.Repository.PostRepository;
 import com.example.BookingTravelBackend.Repository.ReportRepository;
+import com.example.BookingTravelBackend.entity.Post;
 import com.example.BookingTravelBackend.entity.Report;
 import com.example.BookingTravelBackend.payload.respone.ReportRespone;
 import com.example.BookingTravelBackend.service.ReportService;
@@ -14,6 +16,8 @@ import java.util.List;
 
 public class ReportServiceImpl implements ReportService {
     private final ReportRepository reportRepository;
+    private final PostRepository postRepository;
+
     @Override
     public List<ReportRespone> getReport() {
         List<Report> listReport = reportRepository.findAllPending();
@@ -26,4 +30,25 @@ public class ReportServiceImpl implements ReportService {
         }
         return reportResponeList;
     }
+
+    @Override
+    public ReportRespone ReportProcessing(int reportId, String status) {
+        Report report = reportRepository.findById(reportId).orElseThrow(()-> new RuntimeException("Report Not Found"));
+        if(!report.getStatus().equalsIgnoreCase("pending")){
+            throw new RuntimeException("Report Status is not pending");
+        }
+        report.setStatus(status);
+        if (status.equalsIgnoreCase("success")) {
+            Post post = report.getPost();
+            if (post == null) {
+                throw new RuntimeException("Post Not Found");
+            }
+            post.setDelete(true);
+            postRepository.save(post);
+        }
+        Report reportSaved = reportRepository.save(report);
+        return new ReportRespone(reportSaved);
+    }
+
+
 }
